0badf991e9e7c438fdebce9b3566ba2bb88381e5
himanshusalal <himanshusalal517@gmail.com>
2026-05-12 12:48:27 +0530

    start

--- src/components/App.js
+++ src/components/App.js
@@ -16,101 +16,12 @@
 // export default App;
 
 
-import React, { useState } from "react";
+import Flames from "./components/Flames";
 
 function App() {
-  const [name1, setName1] = useState("");
-  const [name2, setName2] = useState("");
-  const [result, setResult] = useState("");
-
-  const calculateFlames = () => {
-    if (!name1 || !name2) {
-      setResult("Please Enter valid input");
-      return;
-    }
-
-    let arr1 = name1.split("");
-    let arr2 = name2.split("");
-
-    // Remove common characters
-    for (let i = 0; i < arr1.length; i++) {
-      let index = arr2.indexOf(arr1[i]);
-
-      if (index !== -1) {
-        arr1[i] = null;
-        arr2[index] = null;
-      }
-    }
-
-    const remaining1 = arr1.filter((char) => char !== null);
-    const remaining2 = arr2.filter((char) => char !== null);
-
-    const count = remaining1.length + remaining2.length;
-
-    const flames = {
-      1: "Friends",
-      2: "Love",
-      3: "Affection",
-      4: "Marriage",
-      5: "Enemy",
-      0: "Siblings",
-    };
-
-    setResult(flames[count % 6]);
-  };
-
-  const clearFields = () => {
-    setName1("");
-    setName2("");
-    setResult("");
-  };
-
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>FLAMES App</h1>
-
-      <input
-        type="text"
-        data-testid="input1"
-        name="name1"
-        placeholder="Enter first name"
-        value={name1}
-        onChange={(e) => setName1(e.target.value)}
-      />
-
-      <br />
-      <br />
-
-      <input
-        type="text"
-        data-testid="input2"
-        name="name2"
-        placeholder="Enter second name"
-        value={name2}
-        onChange={(e) => setName2(e.target.value)}
-      />
-
-      <br />
-      <br />
-
-      <button
-        data-testid="calculate_relationship"
-        name="calculate_relationship"
-        onClick={calculateFlames}
-      >
-        Calculate Relationship Future
-      </button>
-
-      <button
-        data-testid="clear"
-        name="clear"
-        onClick={clearFields}
-        style={{ marginLeft: "10px" }}
-      >
-        Clear
-      </button>
-
-      <h3 data-testid="answer">{result}</h3>
+    <div>
+      <Flames />
     </div>
   );
 }
